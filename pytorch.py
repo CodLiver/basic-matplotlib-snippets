27efaea9944e42cc1ef134da197683ecf05f2e37
Cod Liver (C E S) <cem.ekin.sunal@gmail.com>
2018-11-08 01:01:44 +0000

    transformation and PIL
    transformation and PIL were added.

--- pytorch.py
+++ pytorch.py
@@ -13,3 +13,46 @@ model.eval()
 ##general problems with the load part. You might think what is modelclass.
 # its basically a class that has __init__(), forward(). you need to add your previous model/layers to totally load your model.
 # if you use pretrained resnet etc. NOW I AM SEARCHING HOW TO FIND.
+
+
+# NORMALIZATION OF IMAGES #
+
+#some libraries to import
+from torchvision import transforms
+from PIL import Image
+
+#default model values
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+#transformation values. you should normalize your images before sending to model
+transformations=transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    #in order to fit the images well into our model, we need to have same size images. there are other methods that welcome different size images, but I will explain that later. Haven't found the answer yet. 
+    transforms.Resize(256),
+    transforms.RandomCrop(224),
+    # If this method comes before Resize(size) method, it throws error. ALL images that are inputted in this method, should be in PIL format. Resize Inputs PIL, Outputs PIL. toTensor IN: PIL, OUT: tensor type. 
+    transforms.ToTensor(),
+    #normalize it
+    normalize,
+])
+
+
+##### OPENCV2 IMAGE FORMAT TO PIL FORMAT ###### 
+# sadly sometimes the formats are not compatible :))))))))
+# so this following snippets to change image's cv2 format to PIL format. Useful if you want to normalize as mentioned. If better method outside, please pull request it.
+
+#reads image in cv2 BGR format
+img = cv2.imread(PATH)
+# convets to PIL format. Apparently PIL is in RGB format.
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# cv2 are array, PIL is not array as seen.
+img = Image.fromarray(img)
+# transformation as told.
+img=transformations(img)
+
+## or simply use this. Original PIL open Image.
+img = Image.open(PATHVAL+exampleImage)
+img = transformations(torch.from_numpy(img))
+
+
